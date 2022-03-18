f59ea1c8a7966d1952f7d5ac8397b38d140aef94
Timur <deev.timur@gmail.com>
2022-03-18 17:45:01 +0500

    update site to v 2.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+//Актуальный год в копирайте подвала
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+//Кнопка вверх
+(function () {
+   'use strict';
+
+   function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+
+      if (scrolled > coords) {
+         goTopBtn.classList.add('top-button--show');
+      }
+      if (scrolled < coords) {
+         goTopBtn.classList.remove('top-button--show');
+      }
+   }
+
+   function backToTop() {
+      if (window.pageYOffset > 0) {
+         window.scrollBy(0, -80);
+         setTimeout(backToTop, 0);
+      }
+   }
+
+   var goTopBtn = document.querySelector('.top-button');
+
+   window.addEventListener('scroll', trackScroll);
+   goTopBtn.addEventListener('click', backToTop);
+})();
